c0e0b5155f1757236a529861d935ec90cdcaf805
Jesse Millar <hellojessemillar@gmail.com>
2019-07-07 12:52:03 -0500

    Making the compilation video a bit smaller

--- videos/make-video.sh
+++ videos/make-video.sh
@@ -4,11 +4,14 @@ rm ./*.mkv
 rm ./*.mp4
 
 for file in *.MOV;
-do ffmpeg -i $file -vf scale=640:-1 ${file%.*}-scaled.mkv;
+# Video scale must be a multiple of 1920 (e.g. 640, 480, etc.)
+do ffmpeg -i $file -vf scale=480:-1 ${file%.*}-scaled.mkv;
 done;
 
 ffmpeg -f concat -safe 0 -i <(for f in ./*.mkv; do echo "file '$PWD/$f'"; done) -c copy -an compilation.mp4
 
-ffmpeg -i compilation.mp4 -vframes 1 -f image2 ./compilation.jpg
+ffmpeg -i compilation.mp4 -vframes 1 -f image2 compilation.jpg
 
-mv ./compilation.jpg ../images/compilation.jpg
+mv compilation.jpg ../images/compilation.jpg
+
+rm ./*.mkv
