c8c4ceed85eac655b7546ae133f2476fd5cc15bc
Jesse Millar <hellojessemillar@gmail.com>
2019-07-09 11:08:35 -0500

    Using HandBrake for final encoding

--- videos/make-compilation.sh
+++ videos/make-compilation.sh
@@ -6,17 +6,16 @@ for file in *.MOV
 # Video scale must be a multiple of 1920 (e.g. 640, 480, etc.)
 do
 	ffmpeg -i $file -vf scale=640:-1 ${file%.*}-scaled.mp4
-	ffmpeg -i ${file%.*}-scaled.mp4 -vf crop=640:320 ${file%.*}-cropped.mp4
+	ffmpeg -i ${file%.*}-scaled.mp4 -filter:v "crop=640:320" ${file%.*}-cropped.mp4
+	ffmpeg -i ${file%.*}-cropped.mp4 -q 0 ${file%.*}-final.mts
 done
 
-for f in ./*-cropped.mp4
+for f in ./*-final.mts
 do
 	echo "file '$f'" >> files.txt
 done
 
-ffmpeg -f concat -safe 0 -i files.txt -c copy -an compilation-temp.mp4
-
-ffmpeg -i compilation-temp.mp4 -c copy -movflags faststart compilation.mp4
+ffmpeg -f concat -safe 0 -i files.txt -c copy -an compilation.mp4
 
 ffmpeg -i compilation.mp4 -vframes 1 -f image2 compilation.jpg
 
@@ -24,5 +23,5 @@ mv compilation.jpg ../images/compilation.jpg
 
 rm *-scaled.mp4
 rm *-cropped.mp4
-rm compilation-temp.mp4
+rm *-final.mts
 rm files.txt
