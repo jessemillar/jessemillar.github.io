c852e734bc2e11ca471a7afa7eab630014748328
Jesse Millar <hellojessemillar@gmail.com>
2019-07-09 10:40:01 -0500

    Tweaks

--- videos/make-compilation.sh
+++ videos/make-compilation.sh
@@ -6,10 +6,15 @@ for file in *.MOV
 # Video scale must be a multiple of 1920 (e.g. 640, 480, etc.)
 do
 	ffmpeg -i $file -vf scale=640:-1 ${file%.*}-scaled.mp4
-	ffmpeg -i ${file%.*}-scaled.mp4 -filter:v "crop=640:360" ${file%.*}-cropped.mkv
+	ffmpeg -i ${file%.*}-scaled.mp4 -vf crop=640:320 ${file%.*}-cropped.mp4
 done
 
-ffmpeg -f concat -safe 0 -i <(for f in ./*.mkv; do echo "file '$PWD/$f'"; done) -c copy -an compilation-temp.mp4
+for f in ./*-cropped.mp4
+do
+	echo "file '$f'" >> files.txt
+done
+
+ffmpeg -f concat -safe 0 -i files.txt -c copy -an compilation-temp.mp4
 
 ffmpeg -i compilation-temp.mp4 -c copy -movflags faststart compilation.mp4
 
@@ -17,6 +22,7 @@ ffmpeg -i compilation.mp4 -vframes 1 -f image2 compilation.jpg
 
 mv compilation.jpg ../images/compilation.jpg
 
-#rm *-scaled.mp4
-#rm *.mkv
-#rm compilation-temp.mp4
+rm *-scaled.mp4
+rm *-cropped.mp4
+rm compilation-temp.mp4
+rm files.txt
